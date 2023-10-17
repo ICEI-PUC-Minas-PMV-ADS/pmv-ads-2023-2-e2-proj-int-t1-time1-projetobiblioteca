9d0b7a44d328f0dac5486cc2b4aa812b23e37b45
gabriel <gabrielrussog@hotmail.com>
2023-10-17 11:40:10 -0300

    first commit projet

---
+++ PUCMinas Projeto Biblioteca/Models/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace PUCMinas_Projeto_Biblioteca.Models {
+    public class AppDbContext : DbContext {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        public DbSet<Livros> Livros { get; set; }
+
+
+    }
+}
